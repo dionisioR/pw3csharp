545734cf5b788ec9b81ed33dad4782862c0028ae
dionisioR <rodrigo.b.dionisio@gmail.com>
2025-05-15 10:01:59 -0300

    Update 2025-05-15   10h01
    Update 2025-05-15   10h01

---
+++ 007_introducao_mvc/007_introducao_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _007_introducao_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
